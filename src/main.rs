488c7a22f20b217b9ce72aadc9cd1847280ef6a8
John Pradeep Vincent <yehohanan7@gmail.com>
2020-12-07 18:04:24 +0000

    update sample code

--- src/main.rs
+++ src/main.rs
@@ -6,27 +6,21 @@ async fn main() {
     let uri = "127.0.0.1:7687";
     let user = "neo4j";
     let pass = "neo";
-    let mut graph = Graph::connect(uri, user, pass).await.unwrap();
+    let graph = Graph::connect(uri, user, pass).await.unwrap();
+    let mut result = graph.query("MATCH (p) RETURN p").execute().await.unwrap();
+    //graph
+    //    .query("CREATE (p:Person {name: $name, age: $age}) RETURN p.name, p.age")
+    //    .param("name", "Mr Main")
+    //    .param("age", 37)
+    //    .run()
+    //    .await
+    //    .unwrap();
 
-    graph
-        .query("CREATE (p:Person {name: $name, age: $age}) RETURN p.name, p.age")
-        .param("name", "Mr Main")
-        .param("age", 37)
-        .run()
-        .await
-        .unwrap();
-
-    let mut result = graph
-        .query("MATCH (p:Person) WHERE p.age > 35 RETURN p.name, p.age")
-        .execute()
-        .await
-        .unwrap();
-
+    //let mut result = graph.query("MATCH (p) RETURN p").execute().await.unwrap();
+    let mut count = 0;
     while let Some(row) = result.next().await {
-        println!("========================");
-        let name: String = row.get("p.name").unwrap();
-        let age: i64 = row.get("p.age").unwrap();
-        println!("{:?}, {:?}", name, age);
-        println!("========================");
+        println!("{:?}", row);
+        count += 1;
     }
+    println!("{:?}", count);
 }
