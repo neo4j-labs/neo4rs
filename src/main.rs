65e20d4cfac2da7083e6affd6b82118c90e7c1cf
John Pradeep Vincent <yehohanan7@gmail.com>
2020-12-09 16:36:31 +0000

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,6 @@ use futures::stream::StreamExt;
 use neo4rs::*;
 use std::sync::Arc;
 
-//#[tokio::main]
-//async fn main() {
-//    let uri = "127.0.0.1:7687";
-//    let user = "neo4j";
-//    let pass = "neo";
-//    let graph = Graph::connect(uri, user, pass).await.unwrap();
-//    let mut result = graph.query("MATCH (p) RETURN p").execute().await.unwrap();
-//    //graph
-//    //    .query("CREATE (p:Person {name: $name, age: $age}) RETURN p.name, p.age")
-//    //    .param("name", "Mr Main")
-//    //    .param("age", 37)
-//    //    .run()
-//    //    .await
-//    //    .unwrap();
-//
-//    //let mut result = graph.query("MATCH (p) RETURN p").execute().await.unwrap();
-//    let mut count = 0;
-//    while let Some(row) = result.next().await {
-//        println!("{:?}", row);
-//        count += 1;
-//    }
-//    println!("{:?}", count);
-//}
-//
-//
-
 #[tokio::main]
 async fn main() {
     let uri = "127.0.0.1:7687";
@@ -38,7 +12,10 @@ async fn main() {
     for _ in 1..=5 {
         let graph = graph.clone();
         let handle = tokio::spawn(async move {
-            let mut result = graph.execute(query("MATCH (p) RETURN p")).await.unwrap();
+            let mut result = graph
+                .execute(query("MATCH (p:Person {name: $name}) RETURN p").param("name", "Mr Mark"))
+                .await
+                .unwrap();
             let mut count = 0;
             while let Some(_) = result.next().await {
                 count += 1;
