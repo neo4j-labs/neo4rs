07c9340c7e6179a57e6c30ffb777102c91daa422
Daniel Hauck <mail@dhauck.eu>
2023-02-27 15:51:49 +0100

    Compatibility with Neo4j version 5.

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -377,7 +377,7 @@
 //!    let mut result = graph
 //!        .execute(query(
 //!            "WITH point({ x: 2.3, y: 4.5, crs: 'cartesian' }) AS p1,
-//!             point({ x: 1.1, y: 5.4, crs: 'cartesian' }) AS p2 RETURN distance(p1,p2) AS dist, p1, p2",
+//!             point({ x: 1.1, y: 5.4, crs: 'cartesian' }) AS p2 RETURN point.distance(p1,p2) AS dist, p1, p2",
 //!        ))
 //!        .await
 //!        .unwrap();
