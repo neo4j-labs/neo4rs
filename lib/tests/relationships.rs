c5ee233824f6029cfeb61b28cc5222e7234c14e4
Paul Horn <knutwalker@users.noreply.github.com>
2023-08-04 17:26:31 +0200

    Add keys method to nodes/rels (#101)

--- lib/tests/relationships.rs
+++ lib/tests/relationships.rs
@@ -17,5 +17,6 @@ async fn relationships() {
     assert!(relation.start_node_id() > -1);
     assert!(relation.end_node_id() > -1);
     assert_eq!(relation.typ(), "WORKS_AT");
+    assert_eq!(relation.keys(), vec!["as"]);
     assert_eq!(relation.get::<String>("as").unwrap(), "Engineer");
 }
