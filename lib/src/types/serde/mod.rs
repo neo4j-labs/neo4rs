c7a656f6461aabbe4b7ef09c8088ebdab1e81a51
Paul Horn <dev@knutwalker.engineer>
2023-08-30 16:51:25 +0200

    Don't derive Default for the element extractors

--- lib/src/types/serde/mod.rs
+++ lib/src/types/serde/mod.rs
@@ -16,25 +16,25 @@ mod typ;
 mod urel;
 
 /// Newtype to extract the node id or relationship id during deserialization.
-#[derive(Copy, Clone, Debug, Default, PartialEq, Eq, Hash)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 pub struct Id(pub u64);
 
 /// Newtype to extract the start node id of a relationship during deserialization.
-#[derive(Copy, Clone, Debug, Default, PartialEq, Eq, Hash)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 pub struct StartNodeId(pub u64);
 
 /// Newtype to extract the end node id of a relationship during deserialization.
-#[derive(Copy, Clone, Debug, Default, PartialEq, Eq, Hash)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 pub struct EndNodeId(pub u64);
 
 /// Newtype to extract the node labels during deserialization.
-#[derive(Clone, Debug, Default, PartialEq, Eq, Hash)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct Labels<Coll = Vec<String>>(pub Coll);
 
 /// Newtype to extract the relationship type during deserialization.
-#[derive(Clone, Debug, Default, PartialEq, Eq, Hash)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct Type<T = String>(pub T);
 
 /// Newtype to extract the node property keys during deserialization.
-#[derive(Clone, Debug, Default, PartialEq, Eq)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Keys<Coll = HashSet<String>>(pub Coll);
