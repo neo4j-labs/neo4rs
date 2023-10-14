1675e0d00567129d0781aae16af7a8a791c7b93a
Paul Horn <dev@knutwalker.engineer>
2023-10-14 21:14:33 +0200

    Move test to doc+integration test

--- lib/tests/result_stream.rs
+++ lib/tests/result_stream.rs
@@ -1,53 +1,13 @@
 use neo4rs::*;
 
+mod container;
+
+// The purpose of the test is to not use a `must_use`
+#[allow(unused_must_use)]
 #[tokio::test]
 async fn result_stream() {
-    let graph = Graph::new("127.0.0.1:7687", "neo4j", "neoneoneo")
-        .await
-        .unwrap();
+    let neo4j = container::Neo4jContainer::new().await;
+    let graph = neo4j.graph();
 
-    let before = graph
-        .execute(query("MATCH (n:MyNode) RETURN COUNT(n) AS n"))
-        .await
-        .unwrap()
-        .next()
-        .await
-        .unwrap()
-        .unwrap()
-        .get::<usize>("n")
-        .unwrap();
-
-    // use `run` for fire-and-forget queries, that are being executed on the server
-    graph
-        .run(query("CREATE (n:MyNode {p: 'prop'})"))
-        .await
-        .unwrap();
-
-    // using `execute` without consuming the result will do nothing
-    graph
-        .execute(query("CREATE (n:MyNode {p: 'prop'})"))
-        .await
-        .unwrap();
-
-    // consuming the result stream of`execute` will run the query on the server
-    graph
-        .execute(query("CREATE (n:MyNode {p: 'prop'})"))
-        .await
-        .unwrap()
-        .next()
-        .await
-        .unwrap();
-
-    let after = graph
-        .execute(query("MATCH (n:MyNode) RETURN COUNT(n) AS n"))
-        .await
-        .unwrap()
-        .next()
-        .await
-        .unwrap()
-        .unwrap()
-        .get::<usize>("n")
-        .unwrap();
-
-    assert_eq!(after, before + 2);
+    include!("../include/result_stream.rs");
 }
