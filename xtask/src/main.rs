d5120c6f2bd4fca5bce4d44eb680db20b6bcf2db
Paul Horn <dev@knutwalker.engineer>
2023-10-20 20:58:48 +0200

    Run lockfile updates before releasing

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -13,11 +13,16 @@ type DynError = Box<dyn std::error::Error>;
 type Result<T = ()> = std::result::Result<T, DynError>;
 
 fn try_main() -> Result {
-    let task = env::args().nth(1);
-    match task.as_deref() {
-        Some("msrv") => update_msrv_lock()?,
-        Some("min") => update_min_lock()?,
-        _ => print_help(),
+    let mut tasks = env::args().skip(1);
+    while let Some(task) = tasks.next().as_deref() {
+        match task {
+            "msrv" => update_msrv_lock()?,
+            "min" => update_min_lock()?,
+            _ => {
+                print_help();
+                return Ok(());
+            }
+        }
     }
     Ok(())
 }
@@ -33,37 +38,51 @@ min             Update the MIN lockfile
 }
 
 fn update_msrv_lock() -> Result {
-    let cargo = env::var("CARGO").unwrap_or_else(|_| "cargo".to_string());
+    let Env {
+        lockfile,
+        ci_dir,
+        dry_run,
+        ..
+    } = task_env();
+
     let sh = Shell::new()?;
 
     let pin_versions = [
-        ("chrono".to_owned(), "0.4.23"),
-        ("regex".to_owned(), "1.9.6"),
+        ("chrono".into(), "0.4.23"),
+        ("regex".into(), "1.9.6"),
         (latest_version(&sh, "serde_with")?, "3.1.0"),
         (latest_version(&sh, "time")?, "0.3.20"),
     ];
 
-    cmd!(sh, "rm Cargo.lock").run()?;
+    cmd!(sh, "rm {lockfile}").run_if(dry_run)?;
 
-    for (krate, version) in pin_versions {
-        cmd!(sh, "{cargo} update --package {krate} --precise {version}").run()?;
+    if !pin_versions.is_empty() {
+        let cargo = env::var("CARGO").unwrap_or_else(|_| "cargo".into());
+        for (krate, version) in pin_versions {
+            cmd!(sh, "{cargo} update --package {krate} --precise {version}").run_if(dry_run)?;
+        }
     }
 
-    cmd!(sh, "cargo +1.63.0 test --no-run").run()?;
-    cmd!(sh, "cp Cargo.lock ci/Cargo.lock.msrv").run()?;
+    cmd!(sh, "cargo +1.63.0 test --no-run").run_if(dry_run)?;
+
+    cmd!(sh, "cp {lockfile} {ci_dir}/Cargo.lock.msrv").run_if(dry_run)?;
 
     return Ok(());
 
     fn latest_version(sh: &Shell, krate: &str) -> Result<String> {
-        let dir = match krate.len() {
-            1 => "1".to_owned(),
-            2 => "2".to_owned(),
-            3 => format!("3/{}", &krate[..1]),
-            _ => format!("{}/{}", &krate[..2], &krate[2..4]),
+        let index = match krate.len() {
+            1 => format!("https://index.crates.io/1/{}", krate),
+            2 => format!("https://index.crates.io/2/{}", krate),
+            3 => format!("https://index.crates.io/3/{}/{}", &krate[..1], krate),
+            _ => format!(
+                "https://index.crates.io/{}/{}/{}",
+                &krate[..2],
+                &krate[2..4],
+                krate
+            ),
         };
 
-        // let index = cmd!(sh, "curl --silent https://index.crates.io/{dir}/{krate}").read()?;
-        let index = cmd!(sh, "curl https://index.crates.io/{dir}/{krate}").read()?;
+        let index = cmd!(sh, "curl --silent {index}").read()?;
 
         let version = cmd!(sh, "jq --slurp --raw-output 'map(.vers) | last'")
             .stdin(index)
@@ -74,15 +93,63 @@ fn update_msrv_lock() -> Result {
 }
 
 fn update_min_lock() -> Result {
+    let Env {
+        lockfile,
+        ci_dir,
+        dry_run,
+        ..
+    } = task_env();
+
     let sh = Shell::new()?;
 
-    cmd!(sh, "rm Cargo.lock").run()?;
+    cmd!(sh, "rm {lockfile}").run_if(dry_run)?;
 
     cmd!(sh, "cargo +nightly -Z minimal-versions test --no-run")
         .env("RUST_LOG", "debug")
-        .run()?;
+        .run_if(dry_run)?;
 
-    cmd!(sh, "cp Cargo.lock ci/Cargo.lock.min").run()?;
+    cmd!(sh, "cp {lockfile} {ci_dir}/Cargo.lock.min").run_if(dry_run)?;
 
     Ok(())
+}
+
+struct Env {
+    lockfile: String,
+    ci_dir: String,
+    dry_run: bool,
+}
+
+fn task_env() -> Env {
+    let dry_run = env::var("DRY_RUN")
+        .ok()
+        .and_then(|o| o.parse().ok())
+        .unwrap_or(false);
+
+    let workspace = env::var("WORKSPACE_ROOT");
+
+    let (lockfile, ci_dir) = match workspace {
+        Ok(ws) => (format!("{}/Cargo.lock", ws), format!("{}/ci", ws)),
+        Err(_) => ("Cargo.lock".into(), "ci".into()),
+    };
+
+    Env {
+        lockfile,
+        ci_dir,
+        dry_run,
+    }
+}
+
+trait DryRun {
+    fn run_if(&self, dry_run: bool) -> Result<()>;
+}
+
+impl DryRun for xshell::Cmd<'_> {
+    fn run_if(&self, dry_run: bool) -> Result<()> {
+        if dry_run {
+            eprintln!("DRY_RUN: {}", self);
+        } else {
+            self.run()?;
+        }
+        Ok(())
+    }
 }
