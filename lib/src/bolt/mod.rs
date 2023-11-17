fe9a39d73ec97fb015671ecb5b347b0cce2cf7e3
Paul Horn <dev@knutwalker.engineer>
2023-11-17 19:22:50 +0100

    Implement serializer for packstream

--- lib/src/bolt/mod.rs
+++ lib/src/bolt/mod.rs
@@ -1,9 +1,19 @@
 use bytes::Bytes;
-use serde::de::DeserializeOwned;
+use serde::{de::DeserializeOwned, Serialize};
 
 mod packstream;
 
-pub use packstream::{de, from_bytes};
+pub use packstream::{de, from_bytes, ser, to_bytes};
+
+pub(crate) trait Message: Serialize {
+    fn to_bytes(&self) -> Result<Bytes, ser::Error>;
+}
+
+impl<T: Serialize> Message for T {
+    fn to_bytes(&self) -> Result<Bytes, ser::Error> {
+        packstream::to_bytes(self)
+    }
+}
 
 pub(crate) trait MessageResponse: DeserializeOwned {
     fn parse(bytes: Bytes) -> Result<Self, de::Error>;
@@ -13,4 +23,8 @@ impl<T: DeserializeOwned> MessageResponse for T {
     fn parse(bytes: Bytes) -> Result<Self, de::Error> {
         packstream::from_bytes(bytes)
     }
+}
+
+pub(crate) trait ExpectedResponse {
+    type Response: MessageResponse;
 }
--- lib/src/bolt/packstream.rs
+++ lib/src/bolt/packstream.rs
@@ -3,6 +3,8 @@ use serde::de::DeserializeOwned;
 
 #[path = "de.rs"]
 pub mod de;
+#[path = "ser.rs"]
+pub mod ser;
 
 pub fn from_bytes<T>(mut bytes: Bytes) -> Result<T, de::Error>
 where
@@ -12,4 +14,14 @@ where
     let value = T::deserialize(de)?;
 
     Ok(value)
+}
+
+pub fn to_bytes<T>(value: &T) -> Result<Bytes, ser::Error>
+where
+    T: serde::Serialize,
+{
+    let mut ser = ser::Serializer::empty();
+    value.serialize(&mut ser)?;
+
+    Ok(ser.end())
 }
