8958f241563108885d8cb6e0c55327eec6c387db
Paul Horn <dev@knutwalker.engineer>
2024-07-07 19:44:45 +0200

    Add Discard protocol message

---
+++ lib/src/bolt/request/discard.rs
@@ -0,0 +1,87 @@
+use crate::{
+    bolt::{
+        request::extra::{Extra, WrapExtra},
+        ExpectedResponse, Streaming, Summary,
+    },
+    errors::Result,
+};
+use serde::Serialize;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Discard {
+    extra: Extra,
+}
+
+impl WrapExtra for Discard {
+    fn create(extra: Extra) -> Self {
+        Self { extra }
+    }
+
+    fn extra_mut(&mut self) -> &mut Extra {
+        &mut self.extra
+    }
+}
+
+impl Serialize for Discard {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_newtype_variant("Request", 0x2F, "DISCARD", &self.extra)
+    }
+}
+
+impl ExpectedResponse for Discard {
+    type Response = Summary<Streaming>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::bolt::{packstream::value::bolt, Message as _, MessageResponse as _};
+
+    #[test]
+    fn serialize() {
+        let hello = Discard::some(42).for_query(1);
+        let bytes = hello.to_bytes().unwrap();
+
+        let expected = bolt()
+            .structure(1, 0x2F)
+            .tiny_map(2)
+            .tiny_string("n")
+            .tiny_int(42)
+            .tiny_string("qid")
+            .tiny_int(1)
+            .build();
+
+        assert_eq!(bytes, expected);
+    }
+
+    #[test]
+    fn serialize_default_values() {
+        let hello = Discard::all();
+        let bytes = hello.to_bytes().unwrap();
+
+        let expected = bolt()
+            .structure(1, 0x2F)
+            .tiny_map(1)
+            .tiny_string("n")
+            .tiny_int(-1)
+            .build();
+
+        assert_eq!(bytes, expected);
+    }
+
+    #[test]
+    fn parse() {
+        let data = bolt()
+            .tiny_map(1)
+            .tiny_string("has_more")
+            .bool(true)
+            .build();
+
+        let response = Streaming::parse(data).unwrap();
+
+        assert_eq!(response, Streaming::HasMore);
+    }
+}
--- lib/src/bolt/request/mod.rs
+++ lib/src/bolt/request/mod.rs
@@ -1,3 +1,7 @@
 mod commit;
+mod discard;
+mod extra;
 
 pub use commit::Commit;
+pub use discard::Discard;
+pub use extra::WrapExtra;
