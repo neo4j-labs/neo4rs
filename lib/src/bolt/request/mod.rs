7dc578ccd57d387ba7ccdff1290aed123d847f68
Paul Horn <dev@knutwalker.engineer>
2024-07-07 19:46:07 +0200

    Add Reset protocol message

--- lib/src/bolt/request/mod.rs
+++ lib/src/bolt/request/mod.rs
@@ -3,9 +3,11 @@ mod discard;
 mod extra;
 mod goodbye;
 mod hello;
+mod reset;
 
 pub use commit::Commit;
 pub use discard::Discard;
 pub use extra::WrapExtra;
 pub use goodbye::Goodbye;
 pub use hello::Hello;
+pub use reset::Reset;
