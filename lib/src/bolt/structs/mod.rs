a48f3a1c8e0b13e3e3d5e9ff6a5c7e8d77d14b5f
Paul Horn <dev@knutwalker.engineer>
2024-07-07 18:39:56 +0200

    Add DateTime related Bolt types

--- lib/src/bolt/structs/mod.rs
+++ lib/src/bolt/structs/mod.rs
@@ -1,3 +1,8 @@
 pub use self::date::{Date, DateDuration};
+pub use self::datetime::{
+    DateTime, DateTimeZoneId, DateTimeZoneIdRef, LegacyDateTime, LegacyDateTimeZoneId,
+    LegacyDateTimeZoneIdRef, LocalDateTime,
+};
 mod date;
+mod datetime;
 mod de;
