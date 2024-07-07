fad917a56d204252f1157c34c26c4080d80cb066
Paul Horn <dev@knutwalker.engineer>
2024-07-07 18:40:13 +0200

    Add Duration Bolt type

--- lib/src/bolt/structs/mod.rs
+++ lib/src/bolt/structs/mod.rs
@@ -3,6 +3,8 @@ pub use self::datetime::{
     DateTime, DateTimeZoneId, DateTimeZoneIdRef, LegacyDateTime, LegacyDateTimeZoneId,
     LegacyDateTimeZoneIdRef, LocalDateTime,
 };
+pub use self::duration::Duration;
 mod date;
 mod datetime;
 mod de;
+mod duration;
