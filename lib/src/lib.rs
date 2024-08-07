24a1b18c306bb799ce77db39d725924b4d932368
Paul Horn <knutwalker@users.noreply.github.com>
2024-08-07 21:50:22 +0200

    Return server errors as Neo4jError, not unexpected (#187)

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -438,7 +438,10 @@ mod version;
 
 pub use crate::auth::ClientCertificate;
 pub use crate::config::{Config, ConfigBuilder, Database};
-pub use crate::errors::{unexpected, Error, Result};
+pub use crate::errors::{
+    unexpected, Error, Neo4jClientErrorKind, Neo4jError, Neo4jErrorKind, Neo4jSecurityErrorKind,
+    Result,
+};
 pub use crate::graph::{query, Graph};
 pub use crate::query::Query;
 pub use crate::row::{Node, Path, Point2D, Point3D, Relation, Row, UnboundedRelation};
@@ -455,5 +458,4 @@ pub use crate::types::{
 };
 pub use crate::version::Version;
 
-pub(crate) use errors::Neo4jErrorKind;
 pub(crate) use messages::Success;
