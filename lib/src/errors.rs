d35b29ee23d796883e5aaf3612530117d5dea00b
Paul Horn <dev@knutwalker.engineer>
2024-08-07 22:04:31 +0200

    Mark errors as non_exhaustive

--- lib/src/errors.rs
+++ lib/src/errors.rs
@@ -3,6 +3,7 @@ use crate::DeError;
 pub type Result<T, E = Error> = std::result::Result<T, E>;
 
 #[derive(Debug, thiserror::Error)]
+#[non_exhaustive]
 pub enum Error {
     #[error("an IO error occurred: {detail}")]
     IOError {
