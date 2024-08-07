f606a112cab9adde3b90a3bd096a97522a12c042
Paul Horn <dev@knutwalker.engineer>
2024-08-07 19:53:02 +0200

    Retry on certain query failures for managed transactions
    When using `Graph::run`, `Graph::run_on`, `Graph::execute`, or `Graph::execute_on`, the query might be retries for a while.
    The logic is that all errors with the `Transient` error class as well as a few other error classes are considered retryable.
    This catches errors during a leader election or when the transaction resources on the server (memory, handles, ...) are exhausted.
    Retries happen with an exponential backoff until a retry delay exceeds 60s, at which point the query fails.
    
    Transaction created with `Graph::start_txn` or `Graph::start_txn` are *not* retried automatically, only managed transactions are.

--- lib/src/errors.rs
+++ lib/src/errors.rs
@@ -62,6 +62,112 @@ pub enum Error {
     DeserializationError(DeError),
 }
 
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum Neo4jErrorKind {
+    Client(Neo4jClientErrorKind),
+    Transient,
+    Database,
+    Unknown,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum Neo4jClientErrorKind {
+    Security(Neo4jSecurityErrorKind),
+    SessionExpired,
+    FatalDiscovery,
+    TransactionTerminated,
+    ProtocolViolation,
+    Other,
+    Unknown,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum Neo4jSecurityErrorKind {
+    Authentication,
+    AuthorizationExpired,
+    TokenExpired,
+    Other,
+    Unknown,
+}
+
+impl Neo4jErrorKind {
+    pub(crate) fn new(code: &str) -> Self {
+        let code = Self::adjust_code(code).unwrap_or(code);
+        Self::classify(code)
+    }
+
+    fn adjust_code(code: &str) -> Option<&str> {
+        match code {
+            "Neo.TransientError.Transaction.LockClientStopped" => {
+                Some("Neo.ClientError.Transaction.LockClientStopped")
+            }
+            "Neo.TransientError.Transaction.Terminated" => {
+                Some("Neo.ClientError.Transaction.Terminated")
+            }
+            _ => None,
+        }
+    }
+
+    fn classify(code: &str) -> Self {
+        let mut parts = code.split('.').skip(1);
+        let [class, subclass, kind] = [parts.next(), parts.next(), parts.next()];
+
+        match class {
+            Some("ClientError") => match (subclass, kind) {
+                (Some("Security"), Some("Unauthorized")) => Self::Client(
+                    Neo4jClientErrorKind::Security(Neo4jSecurityErrorKind::Authentication),
+                ),
+                (Some("Security"), Some("AuthorizationExpired")) => Self::Client(
+                    Neo4jClientErrorKind::Security(Neo4jSecurityErrorKind::AuthorizationExpired),
+                ),
+                (Some("Security"), Some("TokenExpired")) => Self::Client(
+                    Neo4jClientErrorKind::Security(Neo4jSecurityErrorKind::TokenExpired),
+                ),
+                (Some("Database"), Some("DatabaseNotFound")) => {
+                    Self::Client(Neo4jClientErrorKind::FatalDiscovery)
+                }
+                (Some("Transaction"), Some("Terminated")) => {
+                    Self::Client(Neo4jClientErrorKind::TransactionTerminated)
+                }
+                (Some("Security"), Some(_)) => Self::Client(Neo4jClientErrorKind::Security(
+                    Neo4jSecurityErrorKind::Other,
+                )),
+                (Some("Security"), _) => Self::Client(Neo4jClientErrorKind::Security(
+                    Neo4jSecurityErrorKind::Unknown,
+                )),
+                (Some("Request"), _) => Self::Client(Neo4jClientErrorKind::ProtocolViolation),
+                (Some("Cluster"), Some("NotALeader")) => {
+                    Self::Client(Neo4jClientErrorKind::SessionExpired)
+                }
+                (Some("General"), Some("ForbiddenOnReadOnlyDatabase")) => {
+                    Self::Client(Neo4jClientErrorKind::SessionExpired)
+                }
+                (Some(_), _) => Self::Client(Neo4jClientErrorKind::Other),
+                _ => Self::Client(Neo4jClientErrorKind::Unknown),
+            },
+            Some("TransientError") => Self::Transient,
+            Some(_) => Self::Database,
+            None => Self::Unknown,
+        }
+    }
+
+    pub(crate) fn can_retry(&self) -> bool {
+        matches!(
+            self,
+            Self::Client(
+                Neo4jClientErrorKind::Security(Neo4jSecurityErrorKind::AuthorizationExpired)
+                    | Neo4jClientErrorKind::SessionExpired
+            ) | Self::Transient
+        )
+    }
+}
+
+impl From<&str> for Neo4jErrorKind {
+    fn from(code: &str) -> Self {
+        Self::new(code)
+    }
+}
+
 impl std::convert::From<deadpool::managed::PoolError<Error>> for Error {
     fn from(e: deadpool::managed::PoolError<Error>) -> Self {
         match e {
