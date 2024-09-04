408aa1c158ae86b23aacf6f7d94875be47831522
Paul Horn <dev@knutwalker.engineer>
2024-09-04 13:50:15 +0200

    Add more variations of StreamingSummary, based on features

--- lib/src/summary.rs
+++ lib/src/summary.rs
@@ -1,15 +1,23 @@
-use std::fmt;
+use std::{fmt, marker::PhantomData};
 
 use serde::{
     de::{self, Visitor},
     Deserialize,
 };
 
-#[derive(Debug, Clone, PartialEq)]
-pub enum Streaming {
-    HasMore,
-    Done(Box<StreamingSummary>),
-}
+#[cfg(feature = "unstable-bolt-protocol-impl-v2")]
+type MapKey = String;
+#[cfg(feature = "unstable-bolt-protocol-impl-v2")]
+type MapValue = crate::bolt::Bolt;
+#[cfg(feature = "unstable-bolt-protocol-impl-v2")]
+type Map = std::collections::HashMap<MapKey, MapValue>;
+
+#[cfg(not(feature = "unstable-bolt-protocol-impl-v2"))]
+type MapKey = crate::BoltString;
+#[cfg(not(feature = "unstable-bolt-protocol-impl-v2"))]
+type MapValue = create::BoltType;
+#[cfg(not(feature = "unstable-bolt-protocol-impl-v2"))]
+type Map = crate::BoltMap;
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum Type {
@@ -17,18 +25,6 @@ pub enum Type {
     Write,
     ReadWrite,
     SchemaOnly,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct StreamingSummary {
-    pub(crate) bookmark: Option<String>,
-    pub(crate) t_last: Option<i64>,
-    pub(crate) r#type: Option<Type>,
-    pub(crate) db: Option<String>,
-    pub(crate) stats: Option<crate::BoltMap>,
-    pub(crate) plan: Option<crate::BoltMap>,
-    pub(crate) profile: Option<crate::BoltMap>,
-    pub(crate) notifications: Option<Vec<crate::BoltMap>>,
 }
 
 impl<'de> Deserialize<'de> for Type {
@@ -77,7 +73,136 @@ impl<'de> Deserialize<'de> for Type {
     }
 }
 
+#[derive(Debug, Clone, PartialEq)]
+pub enum Streaming {
+    HasMore,
+    Done(Box<StreamingSummary>),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct StreamingSummary {
+    pub(crate) bookmark: Option<String>,
+    pub(crate) t_last: Option<i64>,
+    pub(crate) r#type: Option<Type>,
+    pub(crate) db: Option<String>,
+    pub(crate) stats: Option<Map>,
+    pub(crate) plan: Option<Map>,
+    pub(crate) profile: Option<Map>,
+    pub(crate) notifications: Option<Vec<Map>>,
+}
+
 impl<'de> Deserialize<'de> for Streaming {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: de::Deserializer<'de>,
+    {
+        let SummaryBuilder {
+            has_more,
+            bookmark,
+            t_last,
+            r#type,
+            db,
+            stats,
+            plan,
+            profile,
+            notifications,
+        } = SummaryBuilder::<String, Map>::deserialize(deserializer)?;
+
+        if has_more {
+            return Ok(Streaming::HasMore);
+        }
+
+        let full = StreamingSummary {
+            bookmark,
+            t_last,
+            r#type,
+            db,
+            stats,
+            plan,
+            profile,
+            notifications,
+        };
+
+        Ok(Streaming::Done(Box::new(full)))
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum StreamingRef<'de> {
+    HasMore,
+    Done(Box<StreamingSummaryRef<'de>>),
+}
+
+#[cfg(feature = "unstable-bolt-protocol-impl-v2")]
+#[derive(Debug, Clone, PartialEq)]
+pub struct StreamingSummaryRef<'de> {
+    pub(crate) bookmark: Option<&'de str>,
+    pub(crate) t_last: Option<i64>,
+    pub(crate) r#type: Option<Type>,
+    pub(crate) db: Option<&'de str>,
+    pub(crate) stats: Option<std::collections::HashMap<&'de str, crate::bolt::BoltRef<'de>>>,
+    pub(crate) plan: Option<std::collections::HashMap<&'de str, crate::bolt::BoltRef<'de>>>,
+    pub(crate) profile: Option<std::collections::HashMap<&'de str, crate::bolt::BoltRef<'de>>>,
+    pub(crate) notifications:
+        Option<Vec<std::collections::HashMap<&'de str, crate::bolt::BoltRef<'de>>>>,
+}
+
+impl<'de> Deserialize<'de> for StreamingRef<'de> {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: de::Deserializer<'de>,
+    {
+        let SummaryBuilder {
+            has_more,
+            bookmark,
+            t_last,
+            r#type,
+            db,
+            stats,
+            plan,
+            profile,
+            notifications,
+        } = SummaryBuilder::<
+            &'de str,
+            std::collections::HashMap<&'de str, crate::bolt::BoltRef<'de>>,
+        >::deserialize(deserializer)?;
+
+        if has_more {
+            return Ok(StreamingRef::HasMore);
+        }
+
+        let full = StreamingSummaryRef {
+            bookmark,
+            t_last,
+            r#type,
+            db,
+            stats,
+            plan,
+            profile,
+            notifications,
+        };
+
+        Ok(StreamingRef::Done(Box::new(full)))
+    }
+}
+
+#[cfg(feature = "unstable-bolt-protocol-impl-v2")]
+#[derive(Debug, Clone, PartialEq)]
+struct SummaryBuilder<Str, Map> {
+    has_more: bool,
+    bookmark: Option<Str>,
+    t_last: Option<i64>,
+    r#type: Option<Type>,
+    db: Option<Str>,
+    stats: Option<Map>,
+    plan: Option<Map>,
+    profile: Option<Map>,
+    notifications: Option<Vec<Map>>,
+}
+
+impl<'de, Key: Deserialize<'de>, Map: Deserialize<'de>> Deserialize<'de>
+    for SummaryBuilder<Key, Map>
+{
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
         D: de::Deserializer<'de>,
@@ -95,10 +220,10 @@ impl<'de> Deserialize<'de> for Streaming {
             "notifications",
         ];
 
-        struct Visit;
+        struct Visit<Str, Map>(PhantomData<(Str, Map)>);
 
-        impl<'de> Visitor<'de> for Visit {
-            type Value = Streaming;
+        impl<'de, Str: Deserialize<'de>, Map: Deserialize<'de>> Visitor<'de> for Visit<Str, Map> {
+            type Value = SummaryBuilder<Str, Map>;
 
             fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
                 formatter.write_str("a valid streaming response")
@@ -187,11 +312,8 @@ impl<'de> Deserialize<'de> for Streaming {
 
                 let has_more = has_more.unwrap_or(false);
 
-                if has_more {
-                    return Ok(Streaming::HasMore);
-                }
-
-                let full = StreamingSummary {
+                let full = SummaryBuilder {
+                    has_more,
                     bookmark,
                     t_last,
                     r#type,
@@ -202,21 +324,18 @@ impl<'de> Deserialize<'de> for Streaming {
                     notifications,
                 };
 
-                Ok(Streaming::Done(Box::new(full)))
+                Ok(full)
             }
         }
 
-        deserializer.deserialize_struct("Response", FIELDS, Visit)
+        deserializer.deserialize_struct("Response", FIELDS, Visit(PhantomData))
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{
-        packstream::{bolt, from_bytes},
-        BoltMap, BoltString, BoltType,
-    };
+    use crate::packstream::{bolt, from_bytes, from_bytes_ref, Data};
 
     #[test]
     fn parse_stream_summary() {
@@ -228,7 +347,7 @@ mod tests {
 
         let success = from_bytes::<Streaming>(data).unwrap();
 
-        assert!(matches!(success, Streaming::HasMore,));
+        assert!(matches!(success, Streaming::HasMore));
     }
 
     #[test]
@@ -258,10 +377,10 @@ mod tests {
             t_last: Some(42),
             r#type: Some(Type::ReadWrite),
             db: Some("neo4j".to_owned()),
-            stats: Some(BoltMap::from_iter([
-                (BoltString::from("labels-added"), BoltType::from(1)),
-                (BoltString::from("nodes-created"), BoltType::from(2)),
-                (BoltString::from("properties-set"), BoltType::from(3)),
+            stats: Some(Map::from_iter([
+                (MapKey::from("labels-added"), MapValue::from(1)),
+                (MapKey::from("nodes-created"), MapValue::from(2)),
+                (MapKey::from("properties-set"), MapValue::from(3)),
             ])),
             plan: None,
             profile: None,
@@ -271,6 +390,69 @@ mod tests {
         let actual = from_bytes::<Streaming>(data).unwrap();
         let actual = match actual {
             Streaming::Done(actual) => actual,
+            _ => panic!("Expected done"),
+        };
+
+        assert_eq!(*actual, expected);
+    }
+
+    #[cfg(feature = "unstable-bolt-protocol-impl-v2")]
+    #[test]
+    fn parse_stream_summary_ref() {
+        let data = bolt()
+            .tiny_map(1)
+            .tiny_string("has_more")
+            .bool(true)
+            .build();
+
+        let mut data = Data::new(data);
+        let success = from_bytes_ref::<StreamingRef>(&mut data).unwrap();
+
+        assert!(matches!(success, StreamingRef::HasMore));
+    }
+
+    #[cfg(feature = "unstable-bolt-protocol-impl-v2")]
+    #[test]
+    fn parse_full_summary_ref() {
+        let data = bolt()
+            .tiny_map(5)
+            .tiny_string("bookmark")
+            .string16("FB:kcwQ9vYF5wN+TCaprZQJITJbQnaQ")
+            .tiny_string("stats")
+            .tiny_map(3)
+            .tiny_string("labels-added")
+            .tiny_int(1)
+            .tiny_string("nodes-created")
+            .tiny_int(2)
+            .tiny_string("properties-set")
+            .tiny_int(3)
+            .tiny_string("type")
+            .tiny_string("rw")
+            .tiny_string("t_last")
+            .tiny_int(42)
+            .tiny_string("db")
+            .tiny_string("neo4j")
+            .build();
+
+        let expected = StreamingSummaryRef {
+            bookmark: Some("FB:kcwQ9vYF5wN+TCaprZQJITJbQnaQ"),
+            t_last: Some(42),
+            r#type: Some(Type::ReadWrite),
+            db: Some("neo4j"),
+            stats: Some(std::collections::HashMap::from_iter([
+                ("labels-added", crate::bolt::BoltRef::from(1)),
+                ("nodes-created", crate::bolt::BoltRef::from(2)),
+                ("properties-set", crate::bolt::BoltRef::from(3)),
+            ])),
+            plan: None,
+            profile: None,
+            notifications: None,
+        };
+
+        let mut data = Data::new(data);
+        let actual = from_bytes_ref::<StreamingRef>(&mut data).unwrap();
+        let actual = match actual {
+            StreamingRef::Done(actual) => actual,
             _ => panic!("Expected done"),
         };
 
