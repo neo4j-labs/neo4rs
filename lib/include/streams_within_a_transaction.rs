4db8dd78548c5835ee2658657f3cc8fe0da4f197
Paul Horn <knutwalker@users.noreply.github.com>
2025-02-12 10:31:48 +0100

    Simplify the example where two transactions are multiplexed over one connection (#220)

--- lib/include/streams_within_a_transaction.rs
+++ lib/include/streams_within_a_transaction.rs
@@ -1,39 +1,32 @@
-{ 
-    let name = uuid::Uuid::new_v4().to_string();
+{
+    let qry = "UNWIND [1, 2] AS x RETURN x";
+
     let mut txn = graph.start_txn().await.unwrap();
 
-    #[derive(serde::Deserialize)]
-    struct Person {
-        name: String,
-    }
-
-    txn.run_queries([
-        query("CREATE (p { name: $name })").param("name", name.clone()),
-        query("CREATE (p { name: $name })").param("name", name.clone()),
-    ])
-        .await
-        .unwrap();
-
-    //start stream_one
-    let mut stream_one = txn
-        .execute(query("MATCH (p {name: $name}) RETURN p").param("name", name.clone()))
-        .await
-        .unwrap();
+    // start stream_one
+    let mut stream_one = txn.execute(qry).await.unwrap();
     let row = stream_one.next(txn.handle()).await.unwrap().unwrap();
-    assert_eq!(row.to::<Person>().unwrap().name, name);
+    assert_eq!(row.to::<i64>().unwrap(), 1);
 
-    //start stream_two
-    let mut stream_two = txn.execute(query("RETURN 1")).await.unwrap();
+    // start stream_two
+    let mut stream_two = txn.execute(qry).await.unwrap();
     let row = stream_two.next(txn.handle()).await.unwrap().unwrap();
     assert_eq!(row.to::<i64>().unwrap(), 1);
 
-    //stream_one is still active here
+    // stream_one is still active here
     let row = stream_one.next(txn.handle()).await.unwrap().unwrap();
-    assert_eq!(row.to::<Person>().unwrap().name, name);
+    assert_eq!(row.to::<i64>().unwrap(), 2);
 
-    //stream_one completes
+    // as is stream_two
+    let row = stream_two.next(txn.handle()).await.unwrap().unwrap();
+    assert_eq!(row.to::<i64>().unwrap(), 2);
+
+    // stream_one completes
     assert!(stream_one.next(txn.handle()).await.unwrap().is_none());
-    //stream_two completes
+
+    // stream_two completes
     assert!(stream_two.next(txn.handle()).await.unwrap().is_none());
+
+    // commit the transaction
     txn.commit().await.unwrap();
 }
