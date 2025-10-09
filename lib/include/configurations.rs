8b7ef74b5fec8b65bd14c29f90625f85ee520bdd
Paul Horn <paul.horn@neo4j.com>
2025-10-09 14:58:20 +0200

    Move integration tests into their own crate

--- lib/include/configurations.rs
+++
@@ -1,7 +0,0 @@
-{ 
-    let mut result = graph.execute(query("RETURN 1")).await.unwrap();
-    let row = result.next().await.unwrap().unwrap();
-    let value: i64 = row.get("1").unwrap();
-    assert_eq!(1, value);
-    assert!(result.next().await.unwrap().is_none());
-}
